db0959a2a539591346422970b3c728135ba2966b
AiriNarita <airiswim.kitty@gmail.com>
2024-08-03 10:42:13 +0900

    feat: add enum

--- src/main/kotlin/com/example/DevConnect/domain/model/entity/article/ArticleStatus.kt
+++ src/main/kotlin/com/example/DevConnect/domain/model/entity/article/ArticleStatus.kt
@@ -1,6 +1,15 @@
 package com.example.DevConnect.domain.model.entity.article
 
+/**
+ * ArticleStatus 記事ステータスENUMクラス
+ * @property DRAFT 下書き
+ * @property PUBLISHED 公開
+ * @property DELETED 削除済み
+ * @property ARCHIVE アーカイブ(非公開)
+ */
 enum class ArticleStatus {
     DRAFT,
-    PUBLISHED
+    PUBLISHED,
+    DELETED,
+    ARCHIVE
 }
