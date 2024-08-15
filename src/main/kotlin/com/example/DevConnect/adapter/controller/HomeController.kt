7d08879709d334c686e4c96fd924c65b92598c5d
AiriNarita <airiswim.kitty@gmail.com>
2024-08-15 12:15:23 +0900

    backup

--- src/main/kotlin/com/example/DevConnect/adapter/controller/HomeController.kt
+++ src/main/kotlin/com/example/DevConnect/adapter/controller/HomeController.kt
@@ -1,6 +1,9 @@
 package com.example.DevConnect.adapter.controller
 
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
@@ -14,7 +17,11 @@ class HomeController {
      * ホームページの表示
      */
     @GetMapping
-    fun index(): String {
+    fun index(model: Model): String {
+        val authentication: Authentication? = SecurityContextHolder.getContext().authentication
+        val isLoggedIn = authentication?.isAuthenticated == true && authentication?.name != "anonymousUser"
+        model.addAttribute("isLoggedIn", isLoggedIn)
         return "index"
     }
+
 }
