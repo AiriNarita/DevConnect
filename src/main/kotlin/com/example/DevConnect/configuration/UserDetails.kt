6f7db23f81d19d7868e7ea188e671136a052a2f2
AiriNarita <airiswim.kitty@gmail.com>
2024-09-08 23:19:34 +0900

    refactor: remove unuse

--- src/main/kotlin/com/example/DevConnect/configuration/UserDetails.kt
+++ src/main/kotlin/com/example/DevConnect/configuration/UserDetails.kt
@@ -5,10 +5,6 @@ import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
-/**
- * SpringSecurityの関連で追加
- */
-
 data class CustomUserDetails(
     private val user: UserEntity
 ) : UserDetails {
