d2a0ce6143e2fd2ac5b2738c1d20c53b8739e31e
AiriNarita <airiswim.kitty@gmail.com>
2024-09-21 19:40:51 +0900

    fix: add article controller

--- src/main/kotlin/com/example/DevConnect/adapter/controller/ArticleController.kt
+++ src/main/kotlin/com/example/DevConnect/adapter/controller/ArticleController.kt
@@ -4,9 +4,9 @@ import com.example.DevConnect.configuration.CustomUserDetails
 import com.example.DevConnect.domain.model.entity.article.ArticleEntity
 import com.example.DevConnect.infrastructure.dto.ArticleDto
 import com.example.DevConnect.service.ArticleService
-import org.seasar.doma.jdbc.Result
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 
 /**
@@ -18,18 +18,38 @@ import org.springframework.web.bind.annotation.*
 class ArticleController(
     private val articleService: ArticleService
 ) {
+
+    // 記事作成フォームの表示
+    @GetMapping("/new")
+    fun showCreateForm(model: Model): String {
+        return "createArticle"  // createArticle.html テンプレートを返す
+    }
+
+    /**
+     * 記事一覧の表示
+     */
+    @GetMapping("/")
+    fun showArticleList(model: Model): String {
+        val articles = articleService.findAllArticle()
+        model.addAttribute("articles", articles)
+        return "/articles"  // articles.html テンプレートを返す
+    }
+
     /**
      * 記事の登録
      * @param articleDto 記事DTO
      * @return 記事エンティティ
      */
     @PostMapping("")
-    fun create(articleDto: ArticleDto): Result<ArticleEntity> {
+    fun create(
+        @ModelAttribute articleDto: ArticleDto
+    ): String {
         val authentication = SecurityContextHolder.getContext().authentication
         val userId = (authentication.principal as CustomUserDetails).id ?: throw Exception("ユーザーIDが取得できませんでした")
 
         val articleDtoWithUserId = articleDto.copy(userId = userId)
-        return articleService.create(articleDtoWithUserId)
+         articleService.create(articleDtoWithUserId)
+        return "redirect:/articles"
     }
 
     /**
@@ -37,8 +57,12 @@ class ArticleController(
      * @return 記事エンティティ
      */
     @GetMapping("")
-    fun findAllArticle(): List<ArticleEntity> {
-        return articleService.findAllArticle()
+    fun findAllArticle(
+        model: Model
+    ): String {
+        val articles = articleService.findAllArticle()
+        model.addAttribute("articles", articles)
+        return "articles"
     }
 
     /**
